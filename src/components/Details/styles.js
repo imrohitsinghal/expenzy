e2e18d25a788d97072010ea2728ab0f639075238
Rohit Singhal <rohitsinghal91@yahoo.com>
2021-02-28 21:17:22 +0100

    Minor css improvemnets

--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -1,11 +1,12 @@
-import {makeStyles} from '@material-ui/core'
+import {makeStyles, rgbToHex} from '@material-ui/core'
+import { green, red } from '@material-ui/core/colors'
 
 export default makeStyles(()=> ({
     income:{
-        borderBottom: '10px solid rgba(0,255,0,0.5)'
+        borderBottom: '10px solid rgba(0,255,0,0.5)',
     },
     expense:{
-        borderBottom: '10px solid rgba(255,0,0,0.5)'
+        borderBottom: '10px solid rgba(255,0,0,0.5)',
     }
 
 }))
